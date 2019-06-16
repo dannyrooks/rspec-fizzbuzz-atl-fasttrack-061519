505069fe14bd5b9ea9532482727d1ca511dc5516
Daniel Rooker <dannyrooks@gmail.com>
2019-06-16 15:43:41 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(3)
+def fizzbuzz(num)
   if num % 3 == 0
-    puts "Fizz"
+    "Fizz"
   elsif num % 5 == 0
-  puts "Buzz"
+    "Buzz"
   elsif num % 3 && num % 5 == 0
-  puts "Fizzbuzz"
+    "Fizzbuzz"
   end
 end
